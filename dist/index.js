58a01b3c6022f4d623123c7e782baae9e5f85846
Mihael Safaric <mihael.safaric@gmail.com>
2017-09-19 14:15:41 +0200

    Release

--- dist/index.js
+++ dist/index.js
@@ -6,5 +6,6 @@ __export(require("./file-upload/file-select.directive"));
 __export(require("./file-upload/file-drop.directive"));
 __export(require("./file-upload/file-uploader.class"));
 __export(require("./file-upload/file-item.class"));
+__export(require("./file-upload/file-like-object.class"));
 var file_upload_module_1 = require("./file-upload/file-upload.module");
 exports.FileUploadModule = file_upload_module_1.FileUploadModule;
